985a267a7d0e39112df7fc5d5bc4ba38bcb43bdd
Training <bhallashruti512@gmail.com>
2018-05-04 12:19:40 +0530

    Calculator V 1.0

---
+++ java-eclipse/src/com/niit/Calculator.java
@@ -0,0 +1,32 @@
+package com.niit;
+
+public class Calculator {
+
+	Integer a,b;
+
+	public Calculator(Integer a, Integer b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+
+	public Integer getA() {
+		return a;
+	}
+
+	public void setA(Integer a) {
+		this.a = a;
+	}
+
+	public Integer getB() {
+		return b;
+	}
+
+	public void setB(Integer b) {
+		this.b = b;
+	}
+	
+
+	}
+
+
